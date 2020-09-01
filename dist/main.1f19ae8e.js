60e0d89e99e39bd9040dda81b2697547a3cd2287
Maja <majacvetkovic@outlook.com>
2020-09-01 19:22:05 +0200

    Add img  folder and list of destinations

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -5790,11 +5790,205 @@ var global = arguments[3];
 })));
 
 },{}],"main.js":[function(require,module,exports) {
-var moment = require('moment');
+var moment = require('moment'); // require
+
 
 var myDate = new Date();
 console.log(myDate);
 var dateFormating = moment(myDate).format('LL');
+var destinations = [//Visited
+{
+  city: 'Rome',
+  country: 'Italy',
+  photo: 'img/rome.jpg',
+  visited: true
+}, {
+  city: 'Milan',
+  country: 'Italy',
+  photo: 'img/milan.jpg',
+  visited: true
+}, {
+  city: 'Venice',
+  country: 'Italy',
+  photo: 'img/venice.jpg',
+  visited: true
+}, {
+  city: 'Padua',
+  country: 'Italy',
+  photo: 'img/padua.jpg',
+  visited: true
+}, {
+  city: 'Verona',
+  country: 'Italy',
+  photo: 'img/verona.jpg',
+  visited: true
+}, {
+  city: 'Florence',
+  country: 'Italy',
+  photo: 'img/florence.jpg',
+  visited: true
+}, {
+  city: 'Bergamo',
+  country: 'Italy',
+  photo: 'img/bergamo.jpg',
+  visited: true
+}, {
+  city: 'Barcelona',
+  country: 'Spain',
+  photo: 'img/barcelona.jpg',
+  visited: true
+}, {
+  city: 'Lloret de mar',
+  country: 'Spain',
+  photo: 'img/lloretdemar.jpg',
+  visited: true
+}, {
+  city: 'Nice',
+  country: 'France',
+  photo: 'img/nice.jpg',
+  visited: true
+}, {
+  city: 'Cannes',
+  country: 'France',
+  photo: 'img/cannes.jpg',
+  visited: true
+}, {
+  city: 'Stockholm',
+  country: 'Sweden',
+  photo: 'img/stockholm.jpg',
+  visited: true
+}, {
+  city: 'Malmo',
+  country: 'Sweeden',
+  photo: 'img/malmo.jpg',
+  visited: true
+}, {
+  city: 'Copenhagen',
+  country: 'Denmark',
+  photo: 'img/copenhagen.jpg',
+  visited: true
+}, {
+  city: 'Vienna',
+  country: 'Austria',
+  photo: 'img/vienna.jpg',
+  visited: true
+}, {
+  city: 'Monaco',
+  country: 'Monaco',
+  photo: 'img/monaco.jpg',
+  visited: true
+}, {
+  city: 'Budapest',
+  country: 'Hungary',
+  photo: 'img/budapest.jpg',
+  visited: true
+}, {
+  city: 'Prague',
+  country: 'Czech republic',
+  photo: 'img/prague.jpg',
+  visited: true
+}, {
+  city: 'Belgrade',
+  country: 'Serbia',
+  photo: 'img/belgrade.jpg',
+  visited: true
+}, {
+  city: 'Zlatibor',
+  country: 'Serbia',
+  photo: 'img/zlatibor.jpg',
+  visited: true
+}, {
+  city: 'Kopaonik',
+  country: 'Serbia',
+  photo: 'img/kopaonik.jpg',
+  visited: true
+}, {
+  city: 'Bratislava',
+  country: 'Slovakia',
+  photo: 'img/bratislava.jpg',
+  visited: true
+}, {
+  city: 'Sofia',
+  country: 'Bulgaria',
+  photo: 'img/sofia.jpg',
+  visited: true
+}, {
+  city: 'Bansko',
+  country: 'Slovakia',
+  photo: 'img/bansko1.jpg',
+  visited: true
+}, {
+  city: 'Varna',
+  country: 'Slovakia',
+  photo: 'img/varna.jpg',
+  visited: true
+}, {
+  city: 'Thessaloniki',
+  country: 'Greece',
+  photo: 'img/thessaloniki.jpg',
+  visited: true
+}, {
+  city: 'Istanbul',
+  country: 'Turkey',
+  photo: 'img/istanbul.jpg',
+  visited: true
+}, //Wishlist
+{
+  city: 'Paris',
+  country: 'France',
+  photo: 'img/paris.jpg',
+  visited: false
+}, {
+  city: 'Santorini',
+  country: 'Greece',
+  photo: 'img/santorini.jpg',
+  visited: false
+}, {
+  city: 'New York',
+  country: 'U.S.',
+  photo: 'img/newyork.jpg',
+  visited: false
+}, {
+  city: 'Marrakech',
+  country: 'Morocco',
+  photo: 'img/marrakech.jpg',
+  visited: false
+}, {
+  city: 'Amsterdam',
+  country: 'Netherlands',
+  photo: 'img/Amsterdam.png',
+  visited: false
+}, {
+  city: 'Berlin',
+  country: 'Germany',
+  photo: 'img/berlin.jpg',
+  visited: false
+}, {
+  city: 'Lisbon',
+  country: 'Porugal',
+  photo: 'img/lisbon.jpg',
+  visited: false
+}, {
+  city: 'Tokyo',
+  country: 'Japan',
+  photo: 'img/tokyo.jpg',
+  visited: false
+}, {
+  city: 'Sienna',
+  country: 'Italy',
+  photo: 'img/nice.jpg',
+  visited: false
+}, {
+  city: 'Dublin',
+  country: 'Ireland',
+  photo: 'img/dublin.jpg',
+  visited: false
+}, {
+  city: 'Saint petersburg',
+  country: 'Russia',
+  photo: 'img/saintpetersburg.jpg',
+  visited: false
+}];
 },{"moment":"node_modules/moment/moment.js"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -5823,7 +6017,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49532" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53580" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- main.js
+++ main.js
@@ -1,5 +1,244 @@
-var moment = require('moment'); 
+var moment = require('moment'); // require
 
 var myDate = new Date();
 console.log(myDate);
 var dateFormating = moment (myDate).format('LL');
+
+
+let destinations = [
+    
+    //Visited
+
+    {
+      city: 'Rome',
+      country: 'Italy',
+      photo: 'img/rome.jpg',
+      visited: true
+    },
+    {
+      city: 'Milan',
+      country: 'Italy',
+      photo: 'img/milan.jpg',
+      visited: true
+    },
+    {
+      city: 'Venice',
+      country: 'Italy',
+      photo: 'img/venice.jpg',
+      visited: true
+    },
+    {
+      city: 'Padua',
+      country: 'Italy',
+      photo: 'img/padua.jpg',
+      visited: true
+    },
+    {
+      city: 'Verona',
+      country: 'Italy',
+      photo: 'img/verona.jpg',
+      visited: true
+    },
+    {
+      city: 'Florence',
+      country: 'Italy',
+      photo: 'img/florence.jpg',
+      visited: true
+    },
+    {
+      city: 'Bergamo',
+      country: 'Italy',
+      photo: 'img/bergamo.jpg',
+      visited: true
+    },
+    {
+      city: 'Barcelona',
+      country: 'Spain',
+      photo: 'img/barcelona.jpg',
+      visited: true
+    },
+    {
+      city: 'Lloret de mar',
+      country: 'Spain',
+      photo: 'img/lloretdemar.jpg',
+      visited: true
+      },
+    {
+      city: 'Nice',
+      country: 'France',
+      photo: 'img/nice.jpg',
+      visited: true
+    },
+    {
+      city: 'Cannes',
+      country: 'France',
+      photo: 'img/cannes.jpg',
+      visited: true
+    },
+    {
+      city: 'Stockholm',
+      country: 'Sweden',
+      photo: 'img/stockholm.jpg',
+      visited: true
+    },
+    {
+      city: 'Malmo',
+      country: 'Sweeden',
+      photo: 'img/malmo.jpg',
+      visited: true
+    },
+    {
+      city: 'Copenhagen',
+      country: 'Denmark',
+      photo: 'img/copenhagen.jpg',
+      visited: true
+    },
+    {
+      city: 'Vienna',
+      country: 'Austria',
+      photo: 'img/vienna.jpg',
+      visited: true
+    },
+    {
+      city: 'Monaco',
+      country: 'Monaco',
+      photo: 'img/monaco.jpg',
+      visited: true
+    },
+    {
+      city: 'Budapest',
+      country: 'Hungary',
+      photo: 'img/budapest.jpg',
+      visited: true
+    },
+    {
+      city: 'Prague',
+      country: 'Czech republic',
+      photo: 'img/prague.jpg',
+      visited: true
+    },
+    {
+      city: 'Belgrade',
+      country: 'Serbia',
+      photo: 'img/belgrade.jpg',
+      visited: true
+    },
+    {
+      city: 'Zlatibor',
+      country: 'Serbia',
+      photo: 'img/zlatibor.jpg',
+      visited: true
+    },
+    {
+      city: 'Kopaonik',
+      country: 'Serbia',
+      photo: 'img/kopaonik.jpg',
+      visited: true
+    },
+    {
+      city: 'Bratislava',
+      country: 'Slovakia',
+      photo: 'img/bratislava.jpg',
+      visited: true
+    },
+    {
+      city: 'Sofia',
+      country: 'Bulgaria',
+      photo: 'img/sofia.jpg',
+      visited: true
+    },
+    {         
+      city: 'Bansko',
+      country: 'Slovakia',
+      photo: 'img/bansko1.jpg',
+      visited: true
+    },
+    {         
+      city: 'Varna',
+      country: 'Slovakia',
+      photo: 'img/varna.jpg',
+      visited: true
+    },
+    {
+      city: 'Thessaloniki',
+      country: 'Greece',
+      photo: 'img/thessaloniki.jpg',
+      visited: true
+    },
+    {
+      city: 'Istanbul',
+      country: 'Turkey',
+      photo: 'img/istanbul.jpg',
+      visited: true
+    },
+
+    //Wishlist
+
+    {
+        city: 'Paris',
+        country: 'France',
+        photo: 'img/paris.jpg',
+        visited: false
+    },
+    {
+        city: 'Santorini',
+        country: 'Greece',
+        photo: 'img/santorini.jpg',
+        visited: false
+    },
+    {
+        city: 'New York',
+        country: 'U.S.',
+        photo: 'img/newyork.jpg',
+        visited: false
+    },
+    {
+        city: 'Marrakech',
+        country: 'Morocco',
+        photo: 'img/marrakech.jpg',
+        visited: false
+    },
+    {
+        city: 'Amsterdam',
+        country: 'Netherlands',
+        photo: 'img/Amsterdam.png',
+        visited: false
+    },
+    {
+        city: 'Berlin',
+        country: 'Germany',
+        photo: 'img/berlin.jpg',
+        visited: false
+    },
+    {
+        city: 'Lisbon',
+        country: 'Porugal',
+        photo: 'img/lisbon.jpg',
+        visited: false
+    },
+    {
+        city: 'Tokyo',
+        country: 'Japan',
+        photo: 'img/tokyo.jpg',
+        visited: false
+    },
+    {
+        city: 'Sienna',
+        country: 'Italy',
+        photo: 'img/nice.jpg',
+        visited: false
+
+    },
+    {
+        city: 'Dublin',
+        country: 'Ireland',
+        photo: 'img/dublin.jpg',
+        visited: false
+    },
+    {
+        city: 'Saint petersburg',
+        country: 'Russia',
+        photo: 'img/saintpetersburg.jpg',
+        visited: false
+    },
+  ]
