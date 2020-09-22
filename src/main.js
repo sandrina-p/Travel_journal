3105f83ab9b5eb4b68f39876e36c5d4988dbbfc2
Maja <majacvetkovic@outlook.com>
2020-09-22 09:14:01 +0200

    Refactor gallery and buttons

--- src/main.js
+++ src/main.js
@@ -1,30 +1,134 @@
 import moment from "moment";
 import destinationsList from "./scripts/destinationList.js";
 
-//get list of urls
-const getURL = (destinationsList) => {
-  const url = destinationsList.map((item) => item.photo.valueOf());
-  return url;
-};
+// //get list of urls
+// const getURL = (destinationsList) => {
+//   const url = destinationsList.map((item) => item.photo.valueOf());
+//   return url;
+// };
 
-//create html elements for each city
-destinationsList.forEach((destination) => {
-  //create img wrapper
-  const createImg = () => {
-    const figure = document.createElement("figure");
-    figure.classList.add("img-container");
+// const createEls = function () {
+//   return {
+//     figureEl: function () {
+//       const figure = document.createElement("figure");
+//       figure.classList.add("img-container");
+//       document.querySelector(".gallery").appendChild(figure);
+//       return figure;
+//     },
+//     imgEl: function () {
+//       const img = document.createElement("img");
+//       img.style.objectFit = "cover";
+//       figure.appendChild(img);
+//       return img;
+//     },
+//     captionEl: function () {
+//       const figcaption = document.createElement("figcaption");
+//       figcaption.classList.add("caption");
+//       return figcaption;
+//     },
+//   };
+// };
 
-    //create img element
+// const addCityName = () => {
+//   //create elements for city and country names
+//   const cityName = caption.appendChild(document.createElement("h1"));
+//   cityName.classList.add("caption__city");
+
+//   const countryName = caption.appendChild(document.createElement("h2"));
+//   countryName.classList.add("caption__country");
+
+//   figure.appendChild(caption);
+//   return {
+//     cityName,
+//     countryName,
+//   };
+// };
+
+// const cityEls = new createEls();
+// let figure = cityEls.figureEl();
+// let caption = cityEls.captionEl();
+
+// const getCityName = new addCityName();
+// let city = getCityName.cityName;
+// let country = getCityName.countryName;
+
+// //create html elements for each city
+// destinationsList.forEach((destination) => {
+//   //set url for each photo
+//   let cityEl = cityEls.imgEl();
+//   cityEl.src = destination.photo;
+
+//   figure = new cityEls.figureEl();
+//   caption = new cityEls.captionEl();
+
+//   addCityName();
+//   city.innerHTML = destination.city;
+//   country.innerHTML = destination.country;
+
+//   //add the correct class to img wrapper
+
+//   if (destination.visited === true) {
+//     figure.classList.add("visited");
+//   } else {
+//     figure.classList.add("bucketlist");
+//   }
+
+//   return getURL(destinationsList);
+// });
+
+// Object Literal Module
+(function () {
+  // Set the "shared" variables among different methods:
+  let buttons;
+  let visited;
+  let bucketlist;
+  let all;
+  let gallery;
+  let activeBtn;
+
+  //shrink header on scroll
+
+  function cacheDom() {
+    buttons = Array.from(document.querySelector(".buttons").children);
+    visited = buttons[1];
+    bucketlist = buttons[2];
+    all = buttons[3];
+    gallery = document.querySelector(".gallery");
+    activeBtn = null;
+  }
+
+  function bindEvents() {
+    visited.addEventListener("click", addVisited);
+    bucketlist.addEventListener("click", addBucketlist);
+    all.addEventListener("click", showAll);
+    //visited is the default active button on page load
+    document.addEventListener("DOMContentLoaded", setActive);
+    document.addEventListener("scroll", shrinkNav);
+    window.addEventListener("scroll", restoreNav);
+  }
+
+  function renderDestination(destination) {
+    // create destination wrapper
+    const elDestination = document.createElement("figure");
+    elDestination.classList.add("img-container");
+
+    // add the correct class to elDestination wrapper
+    if (destination.visited === true) {
+      elDestination.classList.add("visited");
+    } else {
+      elDestination.classList.add("bucketlist");
+    }
+
+    // create img element
     const img = document.createElement("img");
-    img.style.objectFit = "cover";
-    //add img to figure element
-    document.querySelector(".gallery").appendChild(figure);
-    figure.appendChild(img);
+    img.style.objectFit = "cover"; // TODO - Prefer adding a CSS class instead.
+    img.src = destination.photo;
+    // add img to destination
+    elDestination.appendChild(img);
 
     //set img captions
     const figcaption = document.createElement("figcaption");
     figcaption.classList.add("caption");
-    figure.appendChild(figcaption);
     figcaption.insertAdjacentHTML(
       "beforeend",
       `<h2 class='caption__city'>${destination.city}</h2>`
@@ -33,125 +137,77 @@ destinationsList.forEach((destination) => {
       "beforeend",
       `<h3 class='caption__country'>${destination.country}</h3>`
     );
+    // add captions to destination
+    elDestination.appendChild(figcaption);
 
-    //set url for each photo
-    img.src = destination.photo;
-
-    //add the correct class to img wrapper
-    if (destination.visited === true) {
-      figure.classList.add("visited");
-    } else {
-      figure.classList.add("bucketlist");
-    }
-  };
-
-  return createImg(getURL(destinationsList));
-});
-
-// get buttons
-const visitedButton = document.querySelector(".button__visited");
-const bucketlistButton = document.querySelector(".button__buckelist");
-const allButton = document.querySelector(".button__all");
-//get all photos
-const galleryList = document.querySelector(".gallery");
-
-//set visited cities as default list
-document.addEventListener("DOMContentLoaded", function (event) {
-  document.querySelector(".button__visited").click();
-});
-
-//connect each button with the right list
-const getVisitedCities = visitedButton.addEventListener("click", () => {
-  galleryList.classList.add("visited");
-  galleryList.classList.remove("bucketlist");
-});
-
-const getBucketlistCities = bucketlistButton.addEventListener("click", () => {
-  galleryList.classList.add("bucketlist");
-  galleryList.classList.remove("visited");
-});
-
-const getAllCities = allButton.addEventListener("click", () => {
-  galleryList.classList.remove("bucketlist");
-  galleryList.classList.remove("visited");
-});
-
-//Set active state on clicked button
-const buttons = document.querySelectorAll(".button");
-let activeBtn = null;
-
-const setActiveBtn = buttons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    const current = event.target;
-
-    if (activeBtn) {
-      activeBtn.classList.remove("active-button");
-      activeBtn.setAttribute("aria-pressed", false);
-    }
-
-    current.setAttribute("aria-pressed", true);
-    current.classList.add("active-button");
-
-    activeBtn = current;
-  });
-});
-
-// Object Literal Module
-
-// (function () {
-//   const buttons = {
-//     init: function () {
-//       this.cacheDom();
-//       this.bindEvents();
-//       // this.render();
-//     },
-
-//     cacheDom: function () {
-//       this.buttons = Array.from(document.querySelector(".buttons").children);
-//       this.visited = this.buttons[0];
-//       this.bucketlist = this.buttons[1];
-//       this.all = this.buttons[2];
-//       this.gallery = document.querySelector(".gallery");
-//     },
-
-//     bindEvents: function () {
-//       this.visited.addEventListener("click", addVisited);
-//       this.bucketlist.addEventListener("click", addBucketlist);
-//       this.all.addEventListener("click", showAll);
-//     },
-
-//     addVisited: function () {
-//       this.gallery.classList.add("visited");
-//       this.gallery.classList.remove("bucketlist");
-//     },
-
-//     addBucketlist: function () {
-//       this.gallery.classList.add("bucketlist");
-//       this.gallery.classList.remove("visited");
-//     },
-//     showAll: function () {
-//       this.gallery.classList.remove("visited");
-//       this.gallery.classList.remove("bucketlist");
-//     },
-//   };
-
-//   buttons.init();
-// })();
-
-//shrink header on scroll
-
-const shrinkNav = document.addEventListener("scroll", () => {
-  const navbar = document.querySelector(".navbar");
-  navbar.classList.add("shrink");
-});
-
-//Change header behaviour based on scrolling position
-const navSize = window.addEventListener("scroll", function () {
-  const scrollPos = 0;
-  if (document.body.getBoundingClientRect().top === scrollPos) {
-    document.querySelector(".navbar").classList.remove("shrink");
+    // Return the destination HTML
+    return elDestination;
   }
-});
+
+  function render() {
+    // Create DOM elements for each destination
+    destinationsList.forEach((destination) => {
+      const elDestination = renderDestination(destination);
+      gallery.appendChild(elDestination);
+    });
+  }
+
+  function setActive() {
+    buttons.forEach((button) => {
+      button.addEventListener("click", (event) => {
+        const current = event.target;
+
+        if (activeBtn) {
+          activeBtn.classList.remove("active-button");
+          activeBtn.setAttribute("aria-pressed", false);
+        }
+
+        current.setAttribute("aria-pressed", true);
+        current.classList.add("active-button");
+
+        activeBtn = current;
+      });
+    });
+    document.querySelector(".button__visited").click();
+  }
+
+  function addVisited() {
+    gallery.classList.add("visited");
+    gallery.classList.remove("bucketlist");
+  }
+
+  function addBucketlist() {
+    gallery.classList.add("bucketlist");
+    gallery.classList.remove("visited");
+  }
+
+  function showAll() {
+    gallery.classList.remove("visited");
+    gallery.classList.remove("bucketlist");
+  }
+
+  //shrink header on scroll
+  function shrinkNav() {
+    const navbar = document.querySelector(".navbar");
+    navbar.classList.add("shrink");
+  }
+
+  // restore nav size on scroll up
+  function restoreNav() {
+    const scrollPos = 0;
+    if (document.body.getBoundingClientRect().top === scrollPos) {
+      document.querySelector(".navbar").classList.remove("shrink");
+    }
+  }
+
+  function init() {
+    cacheDom();
+    bindEvents();
+    render();
+  }
+
+  init();
+})();
 
 //create new city
 const addCity = function () {
